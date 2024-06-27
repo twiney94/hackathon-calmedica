d1af73c93a8006e6b0c9d903f88bf9f97b088af9
Yass <61833982+twiney94@users.noreply.github.com>
2024-06-27 12:41:02 +0200

    chore: Update npm dependency to latest stable version

--- frontend/src/utils/http.ts
+++ frontend/src/utils/http.ts
@@ -1,12 +1,16 @@
+import { config } from "dotenv";
+
+config();
+
+const API_BASE_URL = process.env.API_BASE_URL as string;
+
 export const performHttpCall = async (
   path: string,
   method: string = "GET",
   body: any = undefined,
   file: boolean = false
-) => {
+): Promise<any> => {
   try {
-    const API_BASE_URL = process.env.API_BASE_URL;
-
     const headers: Record<string, string> = {
       Accept: file ? "*/*" : "application/json",
     };
