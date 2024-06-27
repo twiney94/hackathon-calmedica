79d0ddd1132de07946fbfa9fe785b00ec2572248
Alexandre Vauthier <alex.vauthier94@gmail.com>
2024-06-27 11:18:00 +0200

    fix user model

--- backend/models/User.go
+++ backend/models/User.go
@@ -1,10 +1,8 @@
 package models
 
-import "github.com/google/uuid"
-
 type User struct {
-	ID          uuid.UUID `gorm:"unique;type:uuid;primaryKey"`
-	FirstName   string    `gorm:"not null" json:"firstName"`
-	LastName    string    `gorm:"not null" json:"lastName"`
-	PhoneNumber string    `gorm:"not null" json:"phoneNumber"`
+	ID          string `gorm:"unique;primaryKey"`
+	FirstName   string `gorm:"not null" json:"firstName"`
+	LastName    string `gorm:"not null" json:"lastName"`
+	PhoneNumber string `gorm:"not null" json:"phoneNumber"`
 }
