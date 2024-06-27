9f52aa78e26d7d9e2e05873e4f6e3947f7a0997b
Yass <61833982+twiney94@users.noreply.github.com>
2024-06-27 10:58:16 +0200

    HTTP calls function for front

---
+++ frontend/src/utils/http.ts
@@ -0,0 +1,41 @@
+export const performHttpCall = async (
+  path: string,
+  method: string = "GET",
+  body: any = undefined,
+  file: boolean = false
+) => {
+  try {
+    const API_BASE_URL = process.env.API_BASE_URL;
+
+    const headers: Record<string, string> = {
+      Accept: file ? "*/*" : "application/json",
+    };
+
+    if (!(body instanceof FormData) && !file) {
+      headers["Content-Type"] = "application/json;charset=UTF-8";
+    }
+
+    const options: RequestInit = {
+      method,
+      headers,
+      body: body instanceof FormData ? body : JSON.stringify(body),
+    };
+
+    const response = await fetch(`${API_BASE_URL}${path}`, options);
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.message || response.statusText);
+    }
+
+    if (file) {
+      const blob = await response.blob();
+      return blob;
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
